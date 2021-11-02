8cea6e3fe338e7334c72d4026afa87d1f71e040b
carlos738 <carlosasl135@hotmail.com>
2021-11-02 02:22:07 -0300

    Add files via upload

---
+++ JogoNãoCadastradoException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiCatalogoDeJogos11.Exceptions
+{
+    public class JogoNãoCadastradoException : Exception
+    {
+        public JogoNãoCadastradoException()
+            :base("Este jogo não está cadastrado")
+        { }
+    }
+}
